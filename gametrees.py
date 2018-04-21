4d03c0cf81a701dc1bd54b6b51afe086bc38997d
nnz138534 <zhangyouheng950913@163.com>
2018-04-21 14:27:00 -0400

    Add files via upload

--- gametrees.py
+++ gametrees.py
@@ -26,29 +26,49 @@ class Gametree(object):
         self.count=3
 
     def build(self):
+        path = os.getcwd()
+        path+="\\root"
+        if os.path.exists(path)==False:
+            os.mkdir(path)
+        os.chdir(path)
         begin=time.time()
         for i in range(2):
             self.root.children.append(BeginNode(self.root,hands="",board="",stack=200,pot=0,current_bet=0,self_bet=0,position=i))
         for node in self.root.children:
             print(time.time()-begin)
             os.system("pause")
-            self.build_subtree(node)
+            self.build_subtree(node,path)
+
 #use recursion to build trees
-    def build_subtree(self,target_node):
+    def build_subtree(self,target_node,path):
         self.count+=1
+        if type(target_node)==BeginNode:
+            subpath=path+"\\Beginnode"+str(self.count)
+        elif type(target_node)==CardNode:
+            if target_node.board=="":
+                subpath=path+"\\"+str([Card.int_to_pretty_str(card)for card in target_node.hands])
+            elif len(target_node.board)==3:
+                subpath = path + "\\" + str([Card.int_to_pretty_str(card) for card in target_node.board])
+            else:
+                subpath = path + "\\" + Card.int_to_pretty_str(target_node.board[-1])
+        elif type(target_node)==ActionNode:
+            subpath=path+"\\("+action_to_str[target_node.label]+","+action_to_str[target_node.previous_action]+")"
+        else:
+            subpath=path+"\\"+target_node.reason
+        if os.path.exists(subpath) == False:
+            os.mkdir(subpath)
+        os.chdir(subpath)
         print(self.count, type(target_node))
-        #if self.count%1000==0: os.system("pause")
         if type(target_node)==TerminalNode:
-            pass
+            with open("node info.txt", "w") as f:
+                f.write(str(target_node))
         else:
             target_node.get_children()
-            #print(self.count,":",target_node)
+            with open("node info.txt", "w") as f:
+                f.write(str(target_node))
             if len(target_node.board)!="":Card.print_pretty_cards(target_node.board)
-               # print(action_to_str[target_node.previous_action])
-                #print("\n",target_node.parent)
-                #os.system("pause")
             for node in target_node.children:
-                self.build_subtree(node)
+                self.build_subtree(node,subpath)
 
 
 """parent:parent node
@@ -71,8 +91,8 @@ class Node(object):
 
     def __str__(self):
         string="type:"+str(type(self))+"\n"
-        string+="hands:"+(self.hands if self.hands==""else str([Card.int_to_pretty_str(card)for card in self.hands]))+"\n"
-        string+="board:"+(self.board if self.board==""else str([Card.int_to_pretty_str(card)for card in self.board]))+"\n"
+        string+="hands:"+(self.hands if self.hands==""else str([Card.int_to_str(card) for card in self.hands]))+"\n"
+        string+="board:"+(self.board if self.board==""else str([Card.int_to_str(card)for card in self.board]))+"\n"
         string+="stack:"+str(self.stack)+"\n"
         string+="pot:"+str(self.pot)+"\n"
         string+="current_bet:"+str(self.current_bet)+"\n"
@@ -102,6 +122,11 @@ class  BeginNode(Node):
             self.self_bet=2
         self.pot+=3
         self.current_bet=2
+
+    def __str__(self):
+        string=Node.__str__(self)
+        string+="position:"+str(self.position)
+        return string
 
     def get_children(self):
         self.children=[]
@@ -183,15 +208,24 @@ previous_action: the action your opponent did(mostly, it won't be used. When ur 
 if there is no previous action,  the default value is Nothing
 """
 class ActionNode(Node):
-    def __init__(self,parent,hands,board,stack,pot,current_bet,self_bet,position,previous_action=Nothing,flip=False):
+    def __init__(self,parent,hands,board,stack,pot,current_bet,self_bet,position,previous_action=Nothing,flip=False,label=-1):
         Node.__init__(self,parent,hands,board,stack,pot,current_bet,self_bet)
         self.position=position
         self.flip=flip
         self.previous_action=previous_action
+        self.label=label
 
     def __str__(self):
         string=Node.__str__(self)
-        #string+="position:"+str(self.position)
+        string+="position:"+str(self.position)+"\n"
+        string += "flip:" + str(self.flip) + "\n"
+        string += "previous action:" + action_to_str[self.previous_action] + "\n"
+        string += "label:" + action_to_str[self.label] + "\n"
+        try:
+            string+=str([self.possibility[i] for i in range(len(self.possibility))])+"\n"
+            string += str([self.regrets[i] for i in range(len(self.regrets))]) + "\n"
+        except AttributeError:
+            pass
         return string
 
     def get_actions(self):
@@ -202,7 +236,7 @@ class ActionNode(Node):
             actions.append(Call)
         if self.current_bet==self.self_bet:
             actions.append(Check)
-        if self.stack>0.5*self.pot and int(0.5*self.pot)!=self.current_bet-self.self_bet:
+        if self.stack>0.5*self.pot and 0.5*self.pot>self.current_bet-self.self_bet:
             actions.append(Halfpot)
         if self.stack>self.pot:
             actions.append(Fullpot)
@@ -234,8 +268,10 @@ class ActionNode(Node):
                                     current_bet=0, self_bet=0, position=self.position))
             #can't flip another card
             else:
-                actions = self.get_actions()
-                for action in actions:
+                self.actions = self.get_actions()
+                self.possibility=[1/len(self.actions)for i in range(len(self.actions))]
+                self.regrets=[0 for i in range(len(self.actions))]
+                for action in self.actions:
                     # if fold, create a terminal node
                     if action == Fold:
                         self.children.append(
@@ -254,13 +290,45 @@ class ActionNode(Node):
                                                               self.pot + self.current_bet - self.self_bet,
                                                               self.current_bet, self.current_bet, self.position,
                                                               reason="Show down"))
+                        elif self.self_bet==1:
+                            temp_actionnode = ActionNode(self, self.hands, self.board, 198, self.pot+1,
+                                                         self.current_bet, self.current_bet, (self.position + 1) % 2,
+                                                         previous_action=Call)
+                            temp_actions=temp_actionnode.get_actions()
+                            for temp_action in temp_actions:
+                                # if the opponent all in,create an actionnode
+                                if temp_action == All_in:
+                                    self.children.append(ActionNode(self, self.hands, self.board, 198,
+                                                                    202, 200,
+                                                                    self.self_bet+1, self.position,
+                                                                    previous_action=All_in, label=Call))
+                                # if the opponent check,create an actionnode which can create cardnode as children
+                                elif temp_action == Check:
+                                        self.children.append(
+                                            ActionNode(self, self.hands, self.board, 198, 4,
+                                                       2, 2, self.position,
+                                                       previous_action=Check, flip=True, label=Call))
+                                # if the opponent bet half of the pot,create an actionnode
+                                elif temp_action == Halfpot:
+                                    self.children.append(ActionNode(self, self.hands, self.board, 198,
+                                                                    6, 4,
+                                                                    2, self.position,
+                                                                    previous_action=Halfpot, label=Call))
+                                # if the opponent bet full pot,create an actionnode
+                                elif temp_action == Fullpot:
+                                    self.children.append(
+                                        ActionNode(self, self.hands, self.board, 198,
+                                                   8, 6,
+                                                   2, self.position, previous_action=Fullpot, label=Check))
+                                else:
+                                    raise NotImplementedError("This call or fold is weird")
                         # if call and there are not 5 borad cards, the game will continue to flip another card and an action node which can have cardnodes as children is created
                         else:
                             self.children.append(ActionNode(self, self.hands, self.board,
                                                             self.stack - (self.current_bet - self.self_bet),
                                                             self.pot + self.current_bet - self.self_bet,
                                                             self.current_bet, self.current_bet, self.position,
-                                                            previous_action=self.previous_action, flip=True))
+                                                            previous_action=self.previous_action, flip=True,label=Call))
                     elif action == Check:
                         # if check ,there are 5 borad cards and no one acts later, the game will end and a terminal node is created
                         if self.position == 0 and len(self.board) == 5:
@@ -271,11 +339,11 @@ class ActionNode(Node):
                         elif self.position == 0 and len(self.board) > 0:
                             self.children.append(
                                 ActionNode(self, self.hands, self.board, self.stack, self.pot, self.current_bet,
-                                           self.self_bet, self.position, previous_action=Check, flip=True))
+                                           self.self_bet, self.position, previous_action=Check, flip=True,label=Check))
                         elif self.position == 1 and len(self.board) == 0:
                             self.children.append(
                                 ActionNode(self, self.hands, self.board, self.stack, self.pot, self.current_bet,
-                                           self.self_bet, self.position, previous_action=Call, flip=True))
+                                           self.self_bet, self.position, previous_action=Call, flip=True,label=Check))
                         # if check and someone will act later, create a series of action nodes to react to different situatins
                         else:
                             # simulate the other one's action node
@@ -295,7 +363,7 @@ class ActionNode(Node):
                                     self.children.append(ActionNode(self, self.hands, self.board, self.stack,
                                                                     self.pot + (200 - self.stack), 200 - self.stack,
                                                                     self.self_bet, self.position,
-                                                                    previous_action=All_in))
+                                                                    previous_action=All_in,label=Check))
                                 # if the opponent check,create an actionnode which can create cardnode as children
                                 elif temp_action == Check:
                                     if len(self.board) == 5:
@@ -307,18 +375,18 @@ class ActionNode(Node):
                                         self.children.append(
                                             ActionNode(self, self.hands, self.board, self.stack, self.pot,
                                                        self.current_bet, self.self_bet, self.position,
-                                                       previous_action=Check, flip=True))
+                                                       previous_action=Check, flip=True,label=Check))
                                 # if the opponent bet half of the pot,create an actionnode
                                 elif temp_action == Halfpot:
                                     self.children.append(ActionNode(self, self.hands, self.board, self.stack,
                                                                     self.pot + int(0.5 * self.pot), int(0.5 * self.pot),
                                                                     self.self_bet, self.position,
-                                                                    previous_action=Halfpot))
+                                                                    previous_action=Halfpot,label=Check))
                                 # if the opponent bet full pot,create an actionnode
                                 elif temp_action == Fullpot:
                                     self.children.append(
                                         ActionNode(self, self.hands, self.board, self.stack, self.pot * 2, self.pot,
-                                                   self.self_bet, self.position, previous_action=Fullpot))
+                                                   self.self_bet, self.position, previous_action=Fullpot ,label=Check))
                                 else:
                                     raise NotImplementedError("This call is weird")
 
@@ -344,7 +412,7 @@ class ActionNode(Node):
                                                self.pot + int(0.5 * self.pot) + 400 - self.pot - self.stack,
                                                self.current_bet + 400 - self.pot - self.stack,
                                                self.self_bet + int(0.5 * self.pot), self.position,
-                                               previous_action=All_in))
+                                               previous_action=All_in, label=Halfpot))
                             elif temp_action == Call or (temp_action == Check and self.board == ""):
                                 if len(self.board) != 5:
                                     self.children.append(
@@ -352,7 +420,7 @@ class ActionNode(Node):
                                                    self.pot + 2 * int(0.5 * self.pot),
                                                    self.self_bet + int(0.5 * self.pot),
                                                    self.self_bet + int(0.5 * self.pot), self.position,
-                                                   previous_action=Call, flip=True))
+                                                   previous_action=Call, flip=True, label=Halfpot))
                                 else:
                                     self.children.append(
                                         TerminalNode(self, self.hands, self.board, self.stack - int(0.5 * self.pot),
@@ -367,14 +435,14 @@ class ActionNode(Node):
                                                    0.5 * (self.pot + int(0.5 * self.pot))),
                                                int(0.5 * (self.pot + int(0.5 * self.pot))) + self.current_bet,
                                                self.self_bet + int(0.5 * self.pot), self.position,
-                                               previous_action=Halfpot))
+                                               previous_action=Halfpot, label=Halfpot))
                             elif temp_action == Fullpot:
                                 self.children.append(
                                     ActionNode(self, self.hands, self.board, self.stack - int(0.5 * self.pot),
                                                2 * (self.pot + int(0.5 * self.pot)),
                                                self.pot + int(0.5 * self.pot) + self.current_bet,
                                                self.self_bet + int(0.5 * self.pot), self.position,
-                                               previous_action=Fullpot))
+                                               previous_action=Fullpot, label=Halfpot))
                             else:
                                 print(self)
                                 print(temp_actionnode)
@@ -398,7 +466,7 @@ class ActionNode(Node):
                                     ActionNode(self, self.hands, self.board, self.stack - self.pot,
                                                2 * self.pot + 400 - self.pot - self.stack,
                                                self.current_bet + 400 - self.pot - self.stack, self.self_bet + self.pot,
-                                               self.position, previous_action=All_in))
+                                               self.position, previous_action=All_in, label=Fullpot))
                             elif temp_action == Call:
                                 if len(self.board) == 5:
                                     self.children.append(
@@ -410,17 +478,17 @@ class ActionNode(Node):
                                     self.children.append(ActionNode(self, self.hands, self.board, self.stack - self.pot,
                                                                     3 * self.pot + self.self_bet - self.current_bet,
                                                                     self.self_bet + self.pot, self.self_bet + self.pot,
-                                                                    self.position, previous_action=Call, flip=True))
+                                                                    self.position, previous_action=Call, flip=True, label=Fullpot))
                             elif temp_action == Halfpot:
                                 self.children.append(
                                     ActionNode(self, self.hands, self.board, self.stack - self.pot, 3 * self.pot,
                                                self.pot + self.current_bet, self.self_bet + self.pot, self.position,
-                                               previous_action=Halfpot))
+                                               previous_action=Halfpot, label=Fullpot))
                             elif temp_action == Fullpot:
                                 self.children.append(
                                     ActionNode(self, self.hands, self.board, self.stack - self.pot, 4 * self.pot,
                                                2 * self.pot + self.current_bet, self.self_bet + self.pot, self.position,
-                                               previous_action=Fullpot))
+                                               previous_action=Fullpot, label=Fullpot))
                             else:
                                 raise NotImplementedError("This check is weird")
                     else:
@@ -435,6 +503,11 @@ class TerminalNode(Node):
         self.position=position
         self.reason=reason
 
+    def __str__(self):
+        string=Node.__str__(self)
+        string+="position:"+str(self.position)+"\n"
+        string+="reason:"+self.reason
+        return string
     def get_actions(self):
         return None
 
